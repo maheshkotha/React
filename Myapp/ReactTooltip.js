ecea4652f6918afab4342b3d5fba95869df8b834
Mahesh Kotha <44569697+maheshkotha@users.noreply.github.com>
2020-08-03 14:18:12 +0530

    React tooltip added

---
+++ Myapp/ReactTooltip.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+
+const Reacttooltip = () => {
+    return (
+        <div>
+            <p 
+   
+                data-tip="Your placeholder"
+                data-place="bottom"
+                data-type="info"
+              
+            >
+                Paragaraph
+            </p>    
+            <ReactTooltip />        
+        </div>
+    )
+}
+
+export default Reacttooltip
