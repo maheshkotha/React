a4e210fa2964bd8509aa6e312fd0f7bdc53e4ac3
Mahesh Kotha <44569697+maheshkotha@users.noreply.github.com>
2020-08-03 20:05:13 +0530

    React color picker added

---
+++ Myapp/ReactColorPicker.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { ChromePicker } from 'react-color';
+
+const ReactColorPicker = () => {
+    const [color, setColor] = useState('#fff');
+    const [showColorPicker, setShowColorPicker] = useState(false);
+
+    return (
+        <div>
+            <button onClick={() => setShowColorPicker(showColorPicker => !showColorPicker)}>
+                {showColorPicker ? 'Close color picker' : 'Pick a color'}
+            </button>
+            {
+                showColorPicker &&   <ChromePicker
+                    color = {color}
+                    onChange= {updateColor => setColor(updateColor.hex)}
+                />
+            }
+
+            <h2>
+                You picked {color}
+            </h2>
+        </div>
+    )
+}
+
+export default ReactColorPicker
